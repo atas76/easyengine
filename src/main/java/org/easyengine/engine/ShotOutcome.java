ffea1ddc728d797a23a0347b5554e89366821e87
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2021-08-01 20:44:21 +0300

    Add labels to shot outcomes

--- src/main/java/org/easyengine/engine/ShotOutcome.java
+++ src/main/java/org/easyengine/engine/ShotOutcome.java
@@ -1,5 +1,25 @@
 package org.easyengine.engine;
 
 public enum ShotOutcome {
-    GOAL, GK, BLK_C, SAVE_C, BLK_R_A, BLK_Gkr, BLK_R_M, SAVE_R_A, SAVE
+
+    GOAL("Goal"),
+    GK("Goal kick"),
+    BLK_C("Corner kick after shot block"),
+    SAVE_C("Corner kick after save"),
+    BLK_R_A("Attacking rebound after shot block"),
+    BLK_Gkr("Goalkeeper recovery after shot block"),
+    BLK_R_M("Distant attacking rebound after shot block"),
+    SAVE_R_A("Attacking rebound after save"),
+    SAVE("Goalkeeper holds after save");
+
+    private String description;
+
+    ShotOutcome(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
 }
