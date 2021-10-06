0fbe8ef014c40249742692e348763953aee5b3bd
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2021-10-06 12:34:08 +0300

    Refine attacking coordinates

--- src/main/java/org/easyengine/engine/space/PitchPosition.java
+++ src/main/java/org/easyengine/engine/space/PitchPosition.java
@@ -1,5 +1,9 @@
 package org.easyengine.engine.space;
 
 public enum PitchPosition {
-    Gkr, GK, D, Dw, M, Mw, A, Aw, C
+    Gkr, GK,
+    D, Dw,
+    M, Mw,
+    A, Aw, Ap, Ag, Ad, Adw, Apw,
+    C
 }
